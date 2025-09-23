3b3e87dac816e9afde1155fb27e284d8cd54f33e
Alexander Ronan <339828832@GoTVDSB.ca>
2025-09-23 08:39:55 -0400

    Add project files.

---
+++ Topic 7.1 - While Loops/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_7._1___While_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
