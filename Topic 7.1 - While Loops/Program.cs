5f6616b8d619c5644347087a49da39b3b4d95e09
Alexander Ronan <339828832@GoTVDSB.ca>
2025-09-24 09:07:37 -0400

    working on coin game

--- Topic 7.1 - While Loops/Program.cs
+++ Topic 7.1 - While Loops/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace Topic_7._1___While_Loops
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Topic_7._1___While_Loops
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int coinOutput;
+            double bet, totalMoney;
+            string gameChoice;
+            
+            Random generator;
+            generator = new Random();
+
+            Console.WriteLine("Flip (f) or quit (q)?");
+            gameChoice = Console.ReadLine();
+
+
+            while (gameChoice != "quit" || gameChoice != "q" && gameChoice == "f" || gameChoice == "flip")
+            {
+                Console.Clear();
+                Console.Write("Enter Bet (Balance: " + totalMoney.ToString("c") + ")" );
+                double.TryParse(Console.ReadLine(), out bet);
+                Console.Clear();
+                Console.WriteLine("You've bet " + bet.ToString("c"));
+
+                Console.WriteLine("Flipping...");
+                coinOutput = generator.Next(2);
+                System.Threading.Thread.Sleep(1000);
+                if (coinOutput == 0)
+                {
+                    Console.WriteLine("You Win! You flipped heads");
+
+                }
+
+       
+                
+            }
+
+          
+
         }
+
+      
     }
+
 }
