5ecc360842d017981daf9024817d60016e1946c7
Alexander Ronan <339828832@GoTVDSB.ca>
2025-09-24 09:39:56 -0400

    ADD HEADS OR TAILS CHOICE

--- Topic 7.1 - While Loops/Program.cs
+++ Topic 7.1 - While Loops/Program.cs
@@ -7,34 +7,67 @@ namespace Topic_7._1___While_Loops
         static void Main(string[] args)
         {
             int coinOutput;
-            double bet, totalMoney;
-            string gameChoice;
+            double bet, totalMoney = 100.00;
+            string gameChoice = "";
             
             Random generator;
             generator = new Random();
 
-            Console.WriteLine("Flip (f) or quit (q)?");
-            gameChoice = Console.ReadLine();
+            Console.WriteLine("Type ENTER to start");
+            Console.ReadLine();
+            Console.Clear();
 
 
-            while (gameChoice != "quit" || gameChoice != "q" && gameChoice == "f" || gameChoice == "flip")
+            while (totalMoney > 0 && gameChoice != "quit" && gameChoice != "q")
             {
-                Console.Clear();
-                Console.Write("Enter Bet (Balance: " + totalMoney.ToString("c") + ")" );
-                double.TryParse(Console.ReadLine(), out bet);
-                Console.Clear();
-                Console.WriteLine("You've bet " + bet.ToString("c"));
-
-                Console.WriteLine("Flipping...");
-                coinOutput = generator.Next(2);
-                System.Threading.Thread.Sleep(1000);
-                if (coinOutput == 0)
+                Console.WriteLine("Flip (f) or Quit (q)?");
+                gameChoice = Console.ReadLine();
+                if (gameChoice == "quit" || gameChoice == "q")
                 {
-                    Console.WriteLine("You Win! You flipped heads");
+                    Console.WriteLine("Thanks for playing!");
+                    Console.ReadLine();
+                }
+                else
+                {
+
+
+                    Console.Clear();
+                    Console.Write("Enter Bet (Balance: " + totalMoney.ToString("c") + ") ");
+                    double.TryParse(Console.ReadLine(), out bet);
+                    while (bet > totalMoney || bet < 0)
+                    {
+                        Console.Write("Enter Bet (Balance: " + totalMoney.ToString("c") + ") ");
+                        double.TryParse(Console.ReadLine(), out bet);
+                    }
+                    Console.Clear();
+                    Console.WriteLine("You've bet " + bet.ToString("c"));
+
+                    Console.WriteLine("Flipping...");
+                    coinOutput = generator.Next(2);
+                    System.Threading.Thread.Sleep(1000);
+
+                    if (coinOutput == 0)
+                    {
+                        Console.WriteLine("You Win! You flipped heads");
+                        Console.ReadLine();
+                        if (bet != 0)
+                            totalMoney = (totalMoney + (bet * 1.5));
+                        Console.Clear();
+                    }
+                    else if (coinOutput == 1)
+                    {
+                        Console.WriteLine("You Lose! You flipped tails.");
+                        Console.ReadLine();
+                        totalMoney = (totalMoney - bet);
+                        if (totalMoney < 0)
+                            totalMoney = 0;
+                        Console.Clear();
+                    }
 
                 }
 
-       
+                Console.WriteLine("Your winnings are " + totalMoney.ToString("c"));
+                Console.ReadLine();
                 
             }
 
